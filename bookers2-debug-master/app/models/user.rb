ab558d6d29c8c7ff028b86c6c36d2c66dfdd604d
3bu1sho <3bu1sho@gmail.com>
2020-12-05 14:33:37 +0000

    owari

--- bookers2-debug-master/app/models/user.rb
+++ bookers2-debug-master/app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy
-  attachment :profile_image
+  has_many :books
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+  attachment :profile_image, destroy: false
 
-    validates :name, uniqueness: true
-    validates :name, length: { maximum: 20 }
-    validates :name, length: { minimum: 2 } 
-    validates :introduction, length: { maximum: 50 }
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 end
